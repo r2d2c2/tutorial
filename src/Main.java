192e14d7318365302bd19aed699351d6d5e57db0
uy6806 <uy6806@naver.com>
2021-06-29 11:10:39 +0900

    start git

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static final int asdf=0;
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.printf("");
+
+        String asdff="안명";
+        String asdfasdf="hello";
+        for (int i = 0; i < 7; i++) {
+
+        }
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}
