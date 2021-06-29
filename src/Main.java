dbfb3bc44ca92e00e1e1d1faa54625724f679808
uy6806 <uy6806@naver.com>
2021-06-29 11:19:26 +0900

    start3 git

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.printf("");
 
         String asdff="안명";
-        String asdfasdf="hellolijhiphijiohjiohjijljib";
+        String asdfasdf="hello321312";
         for (int i = 0; i < 7; i++) {
 
         }
